f376467ca831ca2e800cc5462b67ec4c3d3f8fa0
CDE LABS <michael.campbell@cognizant.com>
2020-02-19 13:08:16 -0600

    branch workflow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          monky
+          ooh ooh aah aah monky
         </p>
         <a
           className="App-link"
